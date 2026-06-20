e0fc573b93970251bec4d33135f0c557b5d55f37
Aelthorim <cobalt.sys@proton.me>
2026-06-20 09:26:58 +0000

    improve error handling and return exit codes on check failures, or parse errors

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,34 @@ use config::*;
 use std::{
     fs,
     io::{self, Write},
+    process::exit,
 };
 fn main() {
     if !fs::exists("isready.toml").unwrap() {
         println!("{}", "Config file not found!".red());
-        return;
+        exit(2);
     }
-    let contents = std::fs::read_to_string("isready.toml").unwrap();
-    let parsed: Config = toml::from_str(&contents).unwrap();
+    let contents = match std::fs::read_to_string("isready.toml") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("{} {}", "Failed to read isready.toml".red(), e);
+            exit(2)
+        }
+    };
+
+    let parsed: Config = match toml::from_str(&contents) {
+        Ok(p) => p,
+        Err(e) => {
+            println!("{} {}", "Failed to parse isready.toml".red(), e);
+            exit(2)
+        }
+    };
     println!(
         "{} {}\n",
         "Running checks for:".bold(),
         parsed.project.name.bright_white()
     );
+    let mut allpassed = true;
     for check in parsed.check {
         print!("{}:  ", check.name().bold());
         io::stdout().flush().unwrap();
@@ -26,7 +41,9 @@ fn main() {
         if run {
             println!("{}", "passed".green())
         } else {
-            println!("{}", "failed".red())
+            println!("{}", "failed".red());
+            allpassed = false
         }
     }
+    exit(if allpassed { 0 } else { 1 })
 }
